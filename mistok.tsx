d5fa2dfdf80d62abc953c46aaf47fe23c9314b9e
Tomo <105588693+neontomo@users.noreply.github.com>
2025-02-25 12:19:22 +0100

    making more typesafe and adding copy button

--- mistok.tsx
+++ mistok.tsx
@@ -1,34 +1,29 @@
-/**
- * Usage:
- * <Mistok vars={{ myObject, myArray, anotherObject }} />
- */
-
 const Mistok = ({ vars }: { vars: object | unknown[] }) => {
-  const varsAsStrings = Object.keys(vars).sort((a, b) => a.length - b.length)
+  /**
+   * Usage:
+   * <Mistok vars={{ myObject, myArray, anotherObject }} />
+   */
 
-  const qAll = (query: string) => document?.querySelectorAll(query)
+  const varNames = Object.keys(vars).sort((a, b) => a.length - b.length)
+  const $ = (q: string) => document?.querySelectorAll(q)
+  // eslint-disable-next-line no-eval
   const jsonEval = (str: string) => JSON.stringify(eval(str), null, 3)
-
-  const updateCurrentViewed = (key: string) => {
-    qAll(`.mistok-pre`).forEach((pre) => pre.classList.add('hidden'))
-    qAll(`.mistok-pre[data-key="${key}"]`)[0].classList.remove('hidden')
-    qAll(`.mistok-btn`).forEach((btn) => btn.classList.remove('active'))
-    qAll(`.mistok-btn[data-key="${key}"]`)[0].classList.add('active')
-  }
-
-  const copyToClipboard = () => {
-    const activeTextContent =
-      qAll('.mistok-pre:not(.hidden)')[0].textContent || ''
-    navigator.clipboard.writeText(jsonEval(JSON.parse(activeTextContent)))
-  }
-
   const buttonStyle =
     'mistok-btn p-2 rounded-lg text-xs cursor-pointer bg-[#21262C] text-[#BCC0C3]'
 
-  const svgMaker = (...elements: JSX.Element[]) => (
+  const updateCurrentViewed = (key: string) => {
+    $(`.mistok-pre,.mistok-debug-btn`).forEach((el) =>
+      el.classList.remove('active')
+    )
+    $(
+      `.mistok-pre[data-key="${key}"],.mistok-debug-btn[data-key="${key}"]`
+    ).forEach((el) => el.classList.add('active'))
+  }
+
+  const svgGen = (...elements: JSX.Element[]) => (
     <svg
-      width="18"
-      height="18"
+      height="14"
+      width="14"
       viewBox="0 0 24 24">
       <g
         fill="none"
@@ -39,7 +34,7 @@ const Mistok = ({ vars }: { vars: object | unknown[] }) => {
     </svg>
   )
 
-  const eyeIcon = svgMaker(
+  const eyeIcon = svgGen(
     <path
       key="eye-p"
       d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
@@ -52,7 +47,7 @@ const Mistok = ({ vars }: { vars: object | unknown[] }) => {
     />
   )
 
-  const clipBoardIcon = svgMaker(
+  const clipBoardIcon = svgGen(
     <rect
       key="clip-r"
       width="8"
@@ -68,45 +63,77 @@ const Mistok = ({ vars }: { vars: object | unknown[] }) => {
     />
   )
 
+  const checkIcon = svgGen(
+    <path
+      stroke-width="2"
+      d="M20 6L9 17l-5-5"
+    />
+  )
+
+  const toggleClipboard = (target: number) => {
+    $('.mistok-copy-btn').forEach((el, i) =>
+      i === target ? el.classList.add('active') : el.classList.remove('active')
+    )
+  }
+
+  const copyToClipboard = () => {
+    const activeTextContent = $('.mistok-pre.active')[0]?.textContent || ''
+    navigator.clipboard.writeText(jsonEval(JSON.parse(activeTextContent)))
+
+    toggleClipboard(1)
+    setTimeout(() => toggleClipboard(0), 1000)
+  }
+
+  const toggleVisibility = () => {
+    $('.mistok-details,.mistok-debug-btns,.mistok-copy-btn')?.forEach((el) =>
+      el.classList.toggle('hidden')
+    )
+    $('.mistok-section')[0]?.classList.toggle('w-125')
+  }
+
   return (
-    <div className="mistok-section fixed top-4 left-4 z-[100] bg-[#0D1116] text-[#d1d3d4] rounded-xl h-auto max-h-125 w-125 shadow-xl text-xs overflow-hidden">
-      <style>{`.mistok-btn.active{background:#5046E5}.mistok-section *{scrollbar-width:none}`}</style>
-      <div className="bg-[#161B22] rounded-t-xl px-6 py-4 flex gap-2 justify-between">
-        <div className="flex gap-2 flex-wrap">
-          {varsAsStrings.map((key, index) => (
+    <div className="mistok-section fixed left-4 top-4 z-[100] h-auto max-h-125 w-125 overflow-hidden rounded-xl bg-[#0D1116] text-xs text-[#d1d3d4] shadow-xl">
+      <style>{`.mistok-pre:not(.active),.mistok-copy-btn:not(.active){display:none}.mistok-debug-btn.active{background:#5046E5}.mistok-section *{scrollbar-width:none}`}</style>
+      <div className="flex w-full justify-between gap-2 rounded-t-xl bg-[#161B22] px-6 py-4">
+        <div className="mistok-debug-btns flex flex-wrap gap-2">
+          {varNames.map((key, i) => (
             <button
               key={key}
-              className={`${buttonStyle} ${index === 0 && 'active'}`}
               data-key={key}
+              className={`mistok-debug-btn ${buttonStyle} ${
+                i === 0 && 'active'
+              }`}
               onClick={() => updateCurrentViewed(key)}>
               {key}
             </button>
           ))}
         </div>
 
-        <div className="flex gap-2 flex-wrap">
+        <div className="flex items-start gap-2">
           <button
-            className={buttonStyle}
+            className={`mistok-copy-btn active ${buttonStyle}`}
             onClick={() => copyToClipboard()}>
             {clipBoardIcon}
           </button>
+          <button className={`mistok-copy-btn ${buttonStyle}`}>
+            {checkIcon}
+          </button>
           <button
             className={buttonStyle}
-            onClick={() =>
-              qAll('.mistok-details')[0].classList.toggle('hidden')
-            }>
+            onClick={() => toggleVisibility()}>
             {eyeIcon}
           </button>
         </div>
       </div>
 
-      <div className="mistok-details h-125 leading-5">
-        {varsAsStrings.map((key, index) => (
+      <div className="mistok-details leading-5 h-125">
+        {varNames.map((key, index) => (
           <pre
+            // eslint-disable-next-line react/no-array-index-key
             key={`${key}-${index}`}
             data-key={key}
-            className={`mistok-pre whitespace-pre-wrap h-full p-6 overflow-x-hidden overflow-y-scroll pb-32 ${
-              index > 0 && 'hidden'
+            className={`mistok-pre h-full overflow-y-scroll whitespace-pre-wrap p-6 pb-32 ${
+              index === 0 && 'active'
             }`}>
             {jsonEval(vars[key as keyof typeof vars] as string)}
           </pre>
