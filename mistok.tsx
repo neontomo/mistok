0255eb86c9dfa558375595f32d4d2e1273f4d0b6
Tomo <105588693+neontomo@users.noreply.github.com>
2025-02-27 20:06:54 +0100

    adding animations + eye closed icon + optimizations to minify

--- mistok.tsx
+++ mistok.tsx
@@ -1,141 +1,89 @@
 const Mistok = ({ vars }: { vars: object | unknown[] }) => {
   /**
-   * Usage:
-   * <Mistok vars={{ myObject, myArray, anotherObject }} />
+   * Usage: <Mistok vars={{ myObject, myArray, anotherObject }} />
    */
 
-  const varNames = Object.keys(vars).sort((a, b) => a.length - b.length)
+  /* eslint-disable @next/next/no-img-element, react/no-array-index-key, no-eval, jsx-a11y/alt-text */
+  type str = string
   const $ = (q: string) => document?.querySelectorAll(q)
-  // eslint-disable-next-line no-eval
-  const jsonEval = (str: string) => JSON.stringify(eval(str), null, 3)
-  const buttonStyle =
-    'mistok-btn p-2 rounded-lg text-xs cursor-pointer bg-[#21262C] text-[#BCC0C3]'
+  const varNames = Object.keys(vars).sort((a, b) => a.length - b.length)
+  const btnStyle =
+    'mstk-btn p-2 rounded-lg cursor-pointer bg-[#21262C] flex gap-1 transition-all'
+  const clipImg = $('.mstk-copy-btn img')[0] as HTMLImageElement
+  const eyeImg = $('.mstk-eye-btn img')[0] as HTMLImageElement
+  const baseImgURL = 'https://gist.githubusercontent.com/neontomo/'
+  const icons = {
+    eye: 'f4d701a17860de85d9701ca5998e3208/raw/bf20de88ebcd7b6aac741b1a614561363564a91d/eyeicon.svg',
+    eyeClosed:
+      '79a57fdacec80ffe94aed0fbc063eac4/raw/7fddda66b4eb55f98b4755a0177c91e474236584/eyeclosed.svg',
+    clip: '00971f9d2ff7cd0b382f5a47ed7d8d5f/raw/a7606bf4083928363a7450b253ee81b13dd720d7/clipboard.svg',
+    check:
+      '6d01eb249d8baa40abd624eeb124d98c/raw/24c5ccc0be6df266dc40d1e8db7acfba3813bd25/checkmarkIcon.svg',
+    json: 'f264dcdfc18cbbb522e6a40eaa4a0654/raw/3062366e0a2cdd9a7ab377f5b9f5a8392682508e/json.svg'
+  }
+  const jsonEval = (s: str) => JSON.stringify(eval(s), null, 3)
+  const imgGen = (s: str) => <img src={`${baseImgURL}${s}`} />
 
-  const updateCurrentViewed = (key: string) => {
-    $(`.mistok-pre,.mistok-debug-btn`).forEach((el) =>
-      el.classList.remove('active')
-    )
-    $(
-      `.mistok-pre[data-key="${key}"],.mistok-debug-btn[data-key="${key}"]`
-    ).forEach((el) => el.classList.add('active'))
+  const updateCurrent = (k: str) => {
+    $('.mstk-pre,.mstk-debug-btn').forEach((el) => {
+      if (el.getAttribute('data-key') === k) el.classList.add('active')
+      else el.classList.remove('active')
+    })
   }
 
-  const svgGen = (...elements: JSX.Element[]) => (
-    <svg
-      height="14"
-      width="14"
-      viewBox="0 0 24 24">
-      <g
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2">
-        {elements}
-      </g>
-    </svg>
-  )
-
-  const eyeIcon = svgGen(
-    <path
-      key="e-p"
-      d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"
-    />,
-    <circle
-      key="e-c"
-      cx="12"
-      cy="12"
-      r="3"
-    />
-  )
-
-  const clipBoardIcon = svgGen(
-    <rect
-      key="c-r"
-      width="8"
-      height="4"
-      x="8"
-      y="2"
-      rx="1"
-      ry="1"
-    />,
-    <path
-      key="c-p"
-      d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"
-    />
-  )
-
-  const checkIcon = svgGen(
-    <path
-      key="ch-p"
-      d="M20 6L9 17l-5-5"
-    />
-  )
-
-  const toggleClipboard = (target: number) => {
-    $('.mistok-copy-btn').forEach((el, i) =>
-      i === target ? el.classList.add('active') : el.classList.remove('active')
-    )
-  }
+  const toggleIcon = (img: HTMLImageElement, m: str, a: str, b: str) =>
+    (img.src = `${baseImgURL}${img.src.includes(m) ? a : b}`)
 
   const copyToClipboard = () => {
-    const activeTextContent = $('.mistok-pre.active')[0]?.textContent || ''
+    const activeTextContent = $('.mstk-pre.active')[0]?.textContent || ''
     navigator.clipboard.writeText(jsonEval(JSON.parse(activeTextContent)))
-
-    toggleClipboard(1)
-    setTimeout(() => toggleClipboard(0), 1000)
+    toggleIcon(clipImg, 'clip', icons.check, icons.clip)
+    setTimeout(() => toggleIcon(clipImg, 'clip', icons.check, icons.clip), 1000)
   }
 
   const toggleVisibility = () => {
-    $('.mistok-details,.mistok-debug-btns,.mistok-copy-btn')?.forEach((el) =>
-      el.classList.toggle('hidden')
-    )
-    $('.mistok-section')[0]?.classList.toggle('w-125')
+    $('.mstk-details')[0].classList.toggle('hidden')
+    toggleIcon(eyeImg, 'closed', icons.eye, icons.eyeClosed)
   }
 
   return (
-    <div className="mistok-section fixed left-4 top-4 z-[100] h-auto max-h-125 w-125 overflow-hidden rounded-xl bg-[#0D1116] text-xs text-[#d1d3d4] shadow-xl">
-      <style>{`.mistok-pre:not(.active),.mistok-copy-btn:not(.active){display:none}.mistok-debug-btn.active{background:#5046E5}.mistok-section *{scrollbar-width:none}`}</style>
-      <div className="flex w-full justify-between gap-2 rounded-t-xl bg-[#161B22] px-6 py-4">
-        <div className="mistok-debug-btns flex flex-wrap gap-2">
-          {varNames.map((key, i) => (
+    <div className="mstk-section fixed top-4 left-4 z-[100] h-auto max-h-[40rem] w-[30rem] overflow-hidden rounded-xl bg-[#0D1116] text-xs text-[#d1d3d4]">
+      <style>{`.mstk-pre:not(.active){display:none}.mstk-debug-btn.active{background:#5046E5}.mstk-section *{scrollbar-width:none}`}</style>
+      <div className="flex w-full justify-between gap-2 bg-[#161B22] p-4">
+        <div className="mstk-debug-btns flex flex-wrap gap-2">
+          {varNames.map((k, i) => (
             <button
-              key={key}
-              data-key={key}
-              className={`mistok-debug-btn ${buttonStyle} ${
-                i === 0 && 'active'
-              }`}
-              onClick={() => updateCurrentViewed(key)}>
-              {key}
+              key={k}
+              data-key={k}
+              className={`mstk-debug-btn ${btnStyle} ${i === 0 && 'active'}`}
+              onClick={() => updateCurrent(k)}>
+              {imgGen(icons.json)}
+              {k}
             </button>
           ))}
         </div>
-
         <div className="flex items-start gap-2">
           <button
-            className={`mistok-copy-btn active ${buttonStyle}`}
+            className={`mstk-copy-btn ${btnStyle}`}
             onClick={() => copyToClipboard()}>
-            {clipBoardIcon}
-          </button>
-          <button className={`mistok-copy-btn ${buttonStyle}`}>
-            {checkIcon}
+            {imgGen(icons.clip)}
           </button>
           <button
-            className={buttonStyle}
+            className={`mstk-eye-btn ${btnStyle}`}
             onClick={() => toggleVisibility()}>
-            {eyeIcon}
+            {imgGen(icons.eyeClosed)}
           </button>
         </div>
       </div>
-
-      <div className="mistok-details leading-5 h-125">
-        {varNames.map((key, index) => (
+      <div className="mstk-details h-[40rem] p-4 leading-5">
+        {varNames.map((k, i) => (
           <pre
-            // eslint-disable-next-line react/no-array-index-key
-            key={`${key}-${index}`}
-            data-key={key}
-            className={`mistok-pre h-full overflow-y-scroll whitespace-pre-wrap p-6 pb-32 ${
-              index === 0 && 'active'
+            key={`${k}-${i}`}
+            data-key={k}
+            className={`mstk-pre h-full overflow-y-scroll pb-32 whitespace-pre-wrap ${
+              i === 0 && 'active'
             }`}>
-            {jsonEval(vars[key as keyof typeof vars] as string)}
+            {jsonEval(vars[k as keyof typeof vars] as str)}
           </pre>
         ))}
       </div>
